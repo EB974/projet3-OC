cc5b73e1639cef8ad3ed813b29ec32dace8cd8c8
Eric_B <eric.bertaut@outlook.fr>
2018-05-21 13:30:01 +0400

    MainActiviry be completed

--- app/src/main/java/com/eric_b/moodtracker/Controllers/HistoryActivity.java
+++ app/src/main/java/com/eric_b/moodtracker/Controllers/HistoryActivity.java
@@ -1,25 +1,126 @@
 package com.eric_b.moodtracker.Controllers;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.eric_b.moodtracker.R;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    // declaration of mood variables
+    SharedPreferences moodPref;
+    public static final String BUNDLE_CURRENT_MOOD = "MOOD";
+    public static final String DATE_CURRENT_MOOD = "DATE_CURRENT_MOOD";
+    public static final String MOOD_CURRENT_MOOD = "MOOD_CURRENT_MOOD";
+    public static final String NOTE_CURRENT_MOOD = "NOTE_CURRENT_MOOD";
+    public static final String DATE_MEM_MOOD = "DATE_MEM_MOOD";
+    public static final String MOOD_MEM_MOOD = "MOOD_MEM_MOOD";
+    public static final String NOTE_MEM_MOOD = "NOTE_MEM_MOOD";
+    ArrayList<Long> dateRec = new ArrayList<>();
+    ArrayList<Integer> moodRec = new ArrayList<>();
+    ArrayList<String> noteRec = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        TextView Txt1Day = findViewById(R.id.day1Text);
+        TextView Txt2Day = findViewById(R.id.day2Text);
+        TextView Txt3Day = findViewById(R.id.day3Text);
+        TextView Txt4Day = findViewById(R.id.day4Text);
+        TextView Txt5Day = findViewById(R.id.day5Text);
+        TextView Txt6Day = findViewById(R.id.day6Text);
+        TextView Txt7Day = findViewById(R.id.day7Text);
+
+        ImageButton noteButton1Day = findViewById(R.id.day1ImageButton);
+        ImageButton noteButton2Day = findViewById(R.id.day2ImageButton);
+        ImageButton noteButton3Day = findViewById(R.id.day3ImageButton);
+        ImageButton noteButton4Day = findViewById(R.id.day4ImageButton);
+        ImageButton noteButton5Day = findViewById(R.id.day5ImageButton);
+        ImageButton noteButton6Day = findViewById(R.id.day6ImageButton);
+        ImageButton noteButton7Day = findViewById(R.id.day7ImageButton);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int widthScreen = metrics.widthPixels;
+        int mood;
+        recoverMemMood();
+        int sizeListMood = dateRec.size();
+        System.out.println("taille liste "+sizeListMood);
+        int intName = 7;
+        for (int i = sizeListMood ; i<=0 ; i--){
+            String nameTxtView="Txt"+Integer.toString(intName)+"Day";
+            //System.out.println(nameTxtView);
+            intName--;
+        }
+        //int widthText = Math.round(widthScreen/100*(20*mood));
+        Txt7Day.setBackgroundResource(R.color.faded_red);
+        Txt7Day.setText("Il y a une semaine");
+        //Txt7Day.setWidth(widthText);
     }
 
-    private void calculPeriod(){
-        java.util.Date date = new java.util.Date();
-        java.util.Date date2 = new java.util.Date(118,4,17);
-        long date3 = date2.getTime() -  date.getTime();
-        System.out.println(date);
-        System.out.println(date2);
-        System.out.println(date3 /86400000);
+    private void recoverMemMood(){
+        moodPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
 
+        // Recover Arraylist of Mood memory
+        if (moodPref.getString(DATE_MEM_MOOD, null) != null) {
+            dateRec = recupMoodDate(DATE_MEM_MOOD);
+            moodRec = recupMoodMood(MOOD_MEM_MOOD);
+            noteRec = recupMoodNote(NOTE_MEM_MOOD);
+        }
     }
+
+    protected ArrayList<Long> recupMoodDate(String MEM_MOOD_INFO){
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+        Gson dateGson = new Gson();
+        ArrayList list= new ArrayList<>();
+        ArrayList<Long> listreturn= new ArrayList<>();
+        String getGson;
+        getGson = moodPref.getString(MEM_MOOD_INFO, null);
+        list = dateGson.fromJson(getGson, list.getClass());
+        for (Object aList : list) {
+            long nb = Math.round((double)aList);
+            System.out.println(aList+"  "+nb);
+            listreturn.add(nb);
+        }
+        return listreturn;}
+
+    protected ArrayList<Integer> recupMoodMood(String MEM_MOOD_INFO) {
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+        Gson dateGson = new Gson();
+        ArrayList list = new ArrayList<>();
+        ArrayList<Integer> listreturn= new ArrayList<>();
+        String getGson;
+        getGson = moodPref.getString(MEM_MOOD_INFO, null);
+        list = dateGson.fromJson(getGson, list.getClass());
+        for (Object aList : list) {
+            int nb = (int)((double)aList);
+            System.out.println(aList+"  "+nb);
+            listreturn.add(nb);
+        }
+        return listreturn;
+    }
+
+    protected ArrayList<String> recupMoodNote(String MEM_MOOD_INFO) {
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+        Gson dateGson = new Gson();
+        ArrayList list = new ArrayList<>();
+        ArrayList<String> listreturn= new ArrayList<>();
+        String getGson;
+        getGson = moodPref.getString(MEM_MOOD_INFO, null);
+        list = dateGson.fromJson(getGson, list.getClass());
+        for (Object aList : list) {
+            listreturn.add((String) aList);
+        }
+        return listreturn;
+    }
+
 }
--- app/src/main/java/com/eric_b/moodtracker/Controllers/MainActivity.java
+++ app/src/main/java/com/eric_b/moodtracker/Controllers/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -16,22 +17,25 @@ import android.widget.RelativeLayout;
 import com.eric_b.moodtracker.Modeles.MoodObject;
 import com.eric_b.moodtracker.R;
 import com.google.gson.Gson;
-
 import java.util.ArrayList;
 import java.util.Calendar;
+
+
 
 public class MainActivity extends AppCompatActivity{
 
 
     // declaration of mood variables
+
+    SharedPreferences daylyMoodPref;
+    SharedPreferences moodPref;
+    public static final String BUNDLE_CURRENT_MOOD = "MOOD";
     public static final String DATE_CURRENT_MOOD = "DATE_CURRENT_MOOD";
     public static final String MOOD_CURRENT_MOOD = "MOOD_CURRENT_MOOD";
     public static final String NOTE_CURRENT_MOOD = "NOTE_CURRENT_MOOD";
     public static final String DATE_MEM_MOOD = "DATE_MEM_MOOD";
     public static final String MOOD_MEM_MOOD = "MOOD_MEM_MOOD";
     public static final String NOTE_MEM_MOOD = "NOTE_MEM_MOOD";
-
-    public static final String BUNDLE_CURRENT_MOOD = "MOOD";
     int mood;
     String dayNote;
 
@@ -40,12 +44,10 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
         ImageButton noteButton = findViewById(R.id.buttonAddNote);
         ImageButton historyButton = findViewById(R.id.buttonHistory);
-
+        daylyMoodPref = getPreferences(MODE_PRIVATE);
+        moodPref = getPreferences(MODE_PRIVATE);
 
         if (savedInstanceState != null) {
             mood = savedInstanceState.getInt(BUNDLE_CURRENT_MOOD);
@@ -56,7 +58,6 @@ public class MainActivity extends AppCompatActivity{
 
         final View currentView;
         currentView = findViewById(R.id.moodScreen);
-
         currentView.setOnTouchListener(
                 new View.OnTouchListener() {
                     int Y1, Y2, deltaY;
@@ -127,7 +128,13 @@ public class MainActivity extends AppCompatActivity{
 
         private void note () {
             LayoutInflater factory = LayoutInflater.from(this);
-            final View alertDialogView = factory.inflate(R.layout.dialog_box, null);
+            ViewGroup viewG = new ViewGroup(this) {
+                @Override
+                protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+                }
+            };
+            final View alertDialogView = factory.inflate(R.layout.dialog_box, viewG);
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setView(alertDialogView);
             builder.setNegativeButton("ANNULER", new DialogInterface.OnClickListener() {
@@ -148,8 +155,8 @@ public class MainActivity extends AppCompatActivity{
         }
 
         private void recMood(){
-            SharedPreferences daylyMoodPref = getPreferences(MODE_PRIVATE);
-            SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+            daylyMoodPref = getPreferences(MODE_PRIVATE);
+            //moodPref = getPreferences(MODE_PRIVATE);
             Calendar now = Calendar.getInstance();
             Calendar moodDate = Calendar.getInstance();
             ArrayList<Long> dateRec = new ArrayList<>();
@@ -158,51 +165,79 @@ public class MainActivity extends AppCompatActivity{
             Gson dateGson = new Gson();
             Gson moodGson = new Gson();
             Gson noteGson = new Gson();
-            String getDateGson,getMoodGson,getNoteGson;
 
             if (daylyMoodPref.getLong(DATE_CURRENT_MOOD, 0) != 0) {
-                 moodDate.setTimeInMillis(daylyMoodPref.getLong(DATE_CURRENT_MOOD, 0));
+                moodDate.setTimeInMillis(daylyMoodPref.getLong(DATE_CURRENT_MOOD, 0));
             }
 
+            // Recover Arraylist of Mood memory
             if (moodPref.getString(DATE_MEM_MOOD, null) != null) {
-                getDateGson = moodPref.getString(DATE_MEM_MOOD, null);
-                getMoodGson = moodPref.getString(MOOD_MEM_MOOD, null);
-                getNoteGson = moodPref.getString(NOTE_MEM_MOOD, null);
-
-                dateRec = dateGson.fromJson(getDateGson, new ArrayList<Long>().getClass());
-                moodRec = moodGson.fromJson(getMoodGson,  new ArrayList<Integer>().getClass());
-                noteRec = noteGson.fromJson(getNoteGson, new ArrayList<String>().getClass());
+                dateRec = recupMoodDate(DATE_MEM_MOOD);
+                moodRec = recupMoodMood(MOOD_MEM_MOOD);
+                noteRec = recupMoodNote(NOTE_MEM_MOOD);
             }
 
+            // put new mood in dayly SharedPreferences
             if (now.get(Calendar.DATE)-moodDate.get(Calendar.DATE)==0){
                 daylyMoodPref.edit().putLong(DATE_CURRENT_MOOD,now.getTime().getTime()).apply();
                 daylyMoodPref.edit().putInt(MOOD_CURRENT_MOOD,mood).apply();
                 daylyMoodPref.edit().putString(NOTE_CURRENT_MOOD,dayNote).apply();
             }
             else{
-               if (dateRec != null) {
-                   dateRec.add(daylyMoodPref.getLong(DATE_CURRENT_MOOD, 0));
-               }
-               if (moodRec != null) {
-                   moodRec.add(daylyMoodPref.getInt(MOOD_CURRENT_MOOD, 0));
-               }
-               if (noteRec != null) {
-                    noteRec.add(daylyMoodPref.getString(NOTE_CURRENT_MOOD, null));
-                }
+                dateRec.add(daylyMoodPref.getLong(DATE_CURRENT_MOOD, 0));
+                moodRec.add(daylyMoodPref.getInt(MOOD_CURRENT_MOOD, 0));
+                noteRec.add(daylyMoodPref.getString(NOTE_CURRENT_MOOD, null));
                 moodPref.edit().putString(DATE_MEM_MOOD,dateGson.toJson(dateRec)).apply();
                 moodPref.edit().putString(MOOD_MEM_MOOD,moodGson.toJson(moodRec)).apply();
                 moodPref.edit().putString(NOTE_MEM_MOOD,noteGson.toJson(noteRec)).apply();
             }
 
-            System.out.println("comparaison :"+(moodDate.get(Calendar.DATE)-now.get(Calendar.DATE)));
-            System.out.println("moodDate :"+moodDate.get(Calendar.DATE));
-            System.out.println("now :"+now.get(Calendar.DATE));
-            System.out.println("humeur : " + mood);
-            System.out.println("note : " + dayNote);
-
-
         }
 
+    protected ArrayList<Long> recupMoodDate(String MEM_MOOD_INFO){
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+        Gson dateGson = new Gson();
+        ArrayList list= new ArrayList<>();
+        ArrayList<Long> listreturn= new ArrayList<>();
+        String getGson;
+        getGson = moodPref.getString(MEM_MOOD_INFO, null);
+        list = dateGson.fromJson(getGson, list.getClass());
+        for (Object aList : list) {
+            long nb = Math.round((double)aList);
+            System.out.println(aList+"  "+nb);
+            listreturn.add(nb);
+        }
+        return listreturn;}
+
+    protected ArrayList<Integer> recupMoodMood(String MEM_MOOD_INFO) {
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+        Gson dateGson = new Gson();
+        ArrayList list = new ArrayList<>();
+        ArrayList<Integer> listreturn= new ArrayList<>();
+        String getGson;
+        getGson = moodPref.getString(MEM_MOOD_INFO, null);
+        list = dateGson.fromJson(getGson, list.getClass());
+        for (Object aList : list) {
+            int nb = (int)((double)aList);
+            System.out.println(aList+"  "+nb);
+            listreturn.add(nb);
+        }
+        return listreturn;
+    }
+
+    protected ArrayList<String> recupMoodNote(String MEM_MOOD_INFO) {
+        SharedPreferences moodPref = getPreferences(MODE_PRIVATE);
+        Gson dateGson = new Gson();
+        ArrayList list = new ArrayList<>();
+        ArrayList<String> listreturn= new ArrayList<>();
+        String getGson;
+        getGson = moodPref.getString(MEM_MOOD_INFO, null);
+        list = dateGson.fromJson(getGson, list.getClass());
+        for (Object aList : list) {
+            listreturn.add((String) aList);
+        }
+        return listreturn;
+    }
 
 }
 
